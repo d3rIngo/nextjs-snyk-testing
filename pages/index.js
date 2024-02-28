5c3f7ad21db194850fc5ff4cca006d9784876a54
d3rIngo <ingo.neubert92@gmx.de>
2024-02-28 14:51:35 +0100

    add: workflow

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to my Next.js application.</p>
+    </div>
+  );
+};
+
+export default Home;
